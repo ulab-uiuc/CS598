bfa8b902b541599e6c6ea74981ecfd918c366eee
chengzr01 <ziruicheng01@gmail.com>
2024-08-28 10:01:06 -0500

    update schedule

--- src/components/home/Schedule.jsx
+++ src/components/home/Schedule.jsx
@@ -3,263 +3,6 @@ import { Jumbotron } from "./migration";
 import { Table } from "react-bootstrap";
 
 const Schedule = () => {
-  const schedule = [
-    {
-      week: 1,
-      date: "Aug 28 Wed",
-      knowledge: "Introduction",
-      research: "Paper reading & analysis",
-      events: "",
-      deadlines: "",
-    },
-    {
-      week: 1,
-      date: "Aug 30 Fri",
-      knowledge: "Graph learning tasks",
-      research: "Paper reading & analysis",
-      events: "Writing task, out",
-      deadlines: "",
-    },
-    {
-      week: 2,
-      date: "Sept 04 Wed",
-      knowledge: '"Shallow" graph learning',
-      research: "Paper reading & analysis",
-      events: "",
-      deadlines: "",
-    },
-    {
-      week: 2,
-      date: "Sept 06 Fri",
-      knowledge: "Graph neural networks: perspective",
-      research: "Paper reading & analysis",
-      events: "",
-      deadlines: "",
-    },
-    {
-      week: 3,
-      date: "Sept 11 Wed",
-      knowledge: "Graph neural networks: model I",
-      research: "Paper reading & analysis",
-      events: "",
-      deadlines: "",
-    },
-    {
-      week: 3,
-      date: "Sept 13 Fri",
-      knowledge: "Graph neural networks: model II",
-      research: "Paper reading & analysis",
-      events: "",
-      deadlines: "Writing task due",
-    },
-    {
-      week: 4,
-      date: "Sept 18 Wed",
-      knowledge: "Paper reading discussions",
-      research: "Ideate & discussion",
-      events: "",
-      deadlines: "",
-    },
-    {
-      week: 4,
-      date: "Sept 20 Fri",
-      knowledge: "Graph neural networks: objective",
-      research: "Ideate & discussion",
-      events: "Proposal task, out",
-      deadlines: "",
-    },
-    {
-      week: 5,
-      date: "Sept 25 Wed",
-      knowledge: "Graph neural networks: pipeline",
-      research: "Ideate & discussion",
-      events: "",
-      deadlines: "",
-    },
-    {
-      week: 5,
-      date: "Sept 27 Fri",
-      knowledge: "Graph neural networks: theory",
-      research: "Ideate & discussion",
-      events: "",
-      deadlines: "",
-    },
-    {
-      week: 6,
-      date: "Oct 02 Wed",
-      knowledge: "Graph neural networks: add-ons",
-      research: "Ideate & discussion",
-      events: "",
-      deadlines: "",
-    },
-    {
-      week: 6,
-      date: "Oct 04 Fri",
-      knowledge: "GNN implementation: PyG & GraphGym",
-      research: "Ideate & discussion",
-      events: "",
-      deadlines: "Proposal due",
-    },
-    {
-      week: 7,
-      date: "Oct 09 Wed",
-      knowledge: "Project idea discussions",
-      research: "Prototype implementation",
-      events: "",
-      deadlines: "",
-    },
-    {
-      week: 7,
-      date: "Oct 11 Fri",
-      knowledge: "Beyond simple graphs: heterogeneous graphs",
-      research: "Prototype implementation",
-      events: "Submission task, out",
-      deadlines: "",
-    },
-    {
-      week: 8,
-      date: "Oct 16 Wed",
-      knowledge: "Beyond simple graphs: knowledge graphs",
-      research: "Prototype implementation",
-      events: "",
-      deadlines: "",
-    },
-    {
-      week: 8,
-      date: "Oct 18 Fri",
-      knowledge: "Beyond simple graphs: knowledge graph reasoning",
-      research: "Prototype implementation",
-      events: "",
-      deadlines: "",
-    },
-    {
-      week: 9,
-      date: "Oct 23 Wed",
-      knowledge: "Beyond prediction: graph generative models",
-      research: "Prototype implementation",
-      events: "",
-      deadlines: "",
-    },
-    {
-      week: 9,
-      date: "Oct 25 Fri",
-      knowledge: "Beyond message passing: expressive GNNs",
-      research: "Prototype implementation",
-      events: "",
-      deadlines: "",
-    },
-    {
-      week: 10,
-      date: "Oct 30 Wed",
-      knowledge: "Beyond small graphs: scale GNNs to large graphs",
-      research: "Paper Writing",
-      events: "",
-      deadlines: "",
-    },
-    {
-      week: 10,
-      date: "Nov 01 Fri",
-      knowledge: "Beyond small GNNs: graph foundation models",
-      research: "Paper Writing",
-      events: "",
-      deadlines: "",
-    },
-    {
-      week: 11,
-      date: "Nov 06 Wed",
-      knowledge: "Beyond sparse graphs: graph transformers",
-      research: "Paper Writing",
-      events: "",
-      deadlines: "",
-    },
-    {
-      week: 11,
-      date: "Nov 08 Fri",
-      knowledge: "GNN applications: recommender systems",
-      research: "Paper Writing",
-      events: "",
-      deadlines: "Submission due",
-    },
-    {
-      week: 12,
-      date: "Nov 13 Wed",
-      knowledge: "GNN applications: graph mining",
-      research: "Review & Response",
-      events: "Review & response task, out",
-      deadlines: "",
-    },
-    {
-      week: 12,
-      date: "Nov 15 Fri",
-      knowledge: "GNN applications: science",
-      research: "Review & Response",
-      events: "",
-      deadlines: "",
-    },
-    {
-      week: 13,
-      date: "Nov 20 Wed",
-      knowledge: "Conclusion",
-      research: "Review & Response",
-      events: "",
-      deadlines: "",
-    },
-    {
-      week: 13,
-      date: "Nov 22 Fri",
-      knowledge: "Course project presentation 1",
-      research: "Review & Response",
-      events: "",
-      deadlines: "",
-    },
-    {
-      week: 14,
-      date: "Dec 04 Wed",
-      knowledge: "Course project presentation 2",
-      research: "Presentation",
-      events: "",
-      deadlines: "",
-    },
-    {
-      week: 14,
-      date: "Dec 06 Fri",
-      knowledge: "Course project presentation 3",
-      research: "Presentation",
-      events: "",
-      deadlines: "Review & response due",
-    },
-  ];
-  const rows = [];
-  let currentWeek = null;
-
-  schedule.forEach((item, index) => {
-    if (item.week !== currentWeek) {
-      currentWeek = item.week;
-      rows.push(
-        <tr key={`week-${item.week}`}>
-          <td rowSpan={2} className="text-center align-middle">
-            {item.week}
-          </td>
-          <td>{item.date}</td>
-          <td>{item.knowledge}</td>
-          <td>{item.research}</td>
-          <td>{item.events}</td>
-          <td>{item.deadlines}</td>
-        </tr>
-      );
-    } else {
-      rows.push(
-        <tr key={`week-${item.week}-${index}`}>
-          <td>{item.date}</td>
-          <td>{item.knowledge}</td>
-          <td>{item.research}</td>
-          <td>{item.events}</td>
-          <td>{item.deadlines}</td>
-        </tr>
-      );
-    }
-  });
-
   return (
     <Jumbotron id="schedule" className="m-0">
       <h2
@@ -281,13 +24,279 @@ const Schedule = () => {
           <tr>
             <th>Week</th>
             <th>Date</th>
-            <th>Knowledge learning</th>
-            <th>Research training</th>
+            <th>Knowledge</th>
+            <th>Research</th>
             <th>Events</th>
             <th>Deadlines</th>
           </tr>
         </thead>
-        <tbody>{rows}</tbody>
+        <tbody>
+          {/* Week 1 */}
+          <tr>
+            <td rowSpan={2} className="text-center align-middle">
+              1
+            </td>
+            <td>Aug 28 Wed</td>
+            <td>Introduction [slides]</td>
+            <td>Paper reading & analysis</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Aug 30 Fri</td>
+            <td>Graph learning tasks</td>
+            <td>Paper reading & analysis</td>
+            <td>Writing task, out</td>
+            <td></td>
+          </tr>
+
+          {/* Week 2 */}
+          <tr>
+            <td rowSpan={2} className="text-center align-middle">
+              2
+            </td>
+            <td>Sept 04 Wed</td>
+            <td>"Shallow" graph learning</td>
+            <td>Paper reading & analysis</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Sept 06 Fri</td>
+            <td>Graph neural networks: perspective</td>
+            <td>Paper reading & analysis</td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          {/* Week 3 */}
+          <tr>
+            <td rowSpan={2} className="text-center align-middle">
+              3
+            </td>
+            <td>Sept 11 Wed</td>
+            <td>Graph neural networks: model I</td>
+            <td>Paper reading & analysis</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Sept 13 Fri</td>
+            <td>Graph neural networks: model II</td>
+            <td>Paper reading & analysis</td>
+            <td></td>
+            <td>Writing task due</td>
+          </tr>
+
+          {/* Week 4 */}
+          <tr>
+            <td rowSpan={2} className="text-center align-middle">
+              4
+            </td>
+            <td>Sept 18 Wed</td>
+            <td>Paper reading discussions</td>
+            <td>Ideate & discussion</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Sept 20 Fri</td>
+            <td>Graph neural networks: objective</td>
+            <td>Ideate & discussion</td>
+            <td>Proposal task, out</td>
+            <td></td>
+          </tr>
+
+          {/* Week 5 */}
+          <tr>
+            <td rowSpan={2} className="text-center align-middle">
+              5
+            </td>
+            <td>Sept 25 Wed</td>
+            <td>Graph neural networks: pipeline</td>
+            <td>Ideate & discussion</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Sept 27 Fri</td>
+            <td>Graph neural networks: theory</td>
+            <td>Ideate & discussion</td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          {/* Week 6 */}
+          <tr>
+            <td rowSpan={2} className="text-center align-middle">
+              6
+            </td>
+            <td>Oct 02 Wed</td>
+            <td>Graph neural networks: add-ons</td>
+            <td>Ideate & discussion</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Oct 04 Fri</td>
+            <td>GNN implementation: PyG & GraphGym</td>
+            <td>Ideate & discussion</td>
+            <td></td>
+            <td>Proposal due</td>
+          </tr>
+
+          {/* Week 7 */}
+          <tr>
+            <td rowSpan={2} className="text-center align-middle">
+              7
+            </td>
+            <td>Oct 09 Wed</td>
+            <td>Project idea discussions</td>
+            <td>Prototype implementation</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Oct 11 Fri</td>
+            <td>Beyond simple graphs: heterogeneous graphs</td>
+            <td>Prototype implementation</td>
+            <td>Submission task, out</td>
+            <td></td>
+          </tr>
+
+          {/* Week 8 */}
+          <tr>
+            <td rowSpan={2} className="text-center align-middle">
+              8
+            </td>
+            <td>Oct 16 Wed</td>
+            <td>Beyond simple graphs: knowledge graphs</td>
+            <td>Prototype implementation</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Oct 18 Fri</td>
+            <td>Beyond simple graphs: knowledge graph reasoning</td>
+            <td>Prototype implementation</td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          {/* Week 9 */}
+          <tr>
+            <td rowSpan={2} className="text-center align-middle">
+              9
+            </td>
+            <td>Oct 23 Wed</td>
+            <td>Beyond prediction: graph generative models</td>
+            <td>Prototype implementation</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Oct 25 Fri</td>
+            <td>Beyond message passing: expressive GNNs</td>
+            <td>Prototype implementation</td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          {/* Week 10 */}
+          <tr>
+            <td rowSpan={2} className="text-center align-middle">
+              10
+            </td>
+            <td>Oct 30 Wed</td>
+            <td>Beyond small graphs: scale GNNs to large graphs</td>
+            <td>Paper Writing</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Nov 01 Fri</td>
+            <td>Beyond small GNNs: graph foundation models</td>
+            <td>Paper Writing</td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          {/* Week 11 */}
+          <tr>
+            <td rowSpan={2} className="text-center align-middle">
+              11
+            </td>
+            <td>Nov 06 Wed</td>
+            <td>Beyond sparse graphs: graph transformers</td>
+            <td>Paper Writing</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Nov 08 Fri</td>
+            <td>GNN applications: recommender systems</td>
+            <td>Paper Writing</td>
+            <td></td>
+            <td>Submission due</td>
+          </tr>
+
+          {/* Week 12 */}
+          <tr>
+            <td rowSpan={2} className="text-center align-middle">
+              12
+            </td>
+            <td>Nov 13 Wed</td>
+            <td>GNN applications: graph mining</td>
+            <td>Review & Response</td>
+            <td>Review & response task, out</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Nov 15 Fri</td>
+            <td>GNN applications: science</td>
+            <td>Review & Response</td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          {/* Week 13 */}
+          <tr>
+            <td rowSpan={2} className="text-center align-middle">
+              13
+            </td>
+            <td>Nov 20 Wed</td>
+            <td>Conclusion</td>
+            <td>Review & Response</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Nov 22 Fri</td>
+            <td>Course project presentation 1</td>
+            <td>Review & Response</td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          {/* Week 14 */}
+          <tr>
+            <td rowSpan={2} className="text-center align-middle">
+              14
+            </td>
+            <td>Dec 04 Wed</td>
+            <td>Course project presentation 2</td>
+            <td>Presentation</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Dec 06 Fri</td>
+            <td>Course project presentation 3</td>
+            <td>Presentation</td>
+            <td></td>
+            <td>Review & response due</td>
+          </tr>
+        </tbody>
       </Table>
     </Jumbotron>
   );
