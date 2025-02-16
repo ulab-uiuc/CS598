0968b3f63a2f37532a542b8b6c16e916f4c0b978
chengzr01 <ziruicheng01@gmail.com>
2025-02-16 15:19:07 -0600

    update

--- src/components/home/MainBody.jsx
+++ src/components/home/MainBody.jsx
@@ -13,19 +13,19 @@ const MainBody = React.forwardRef(({}, ref) => {
         backgroundImage: `url(${Siebel})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
-        height: "20em",
+        height: "15em",
       }}
       className="title d-flex align-content-center align-items-center flex-wrap m-0"
     >
       <div id="stars"></div>
       <Container className="text-center">
-        <b
+        <div
           ref={ref}
           className="display-3"
           style={{ marginTop: "1em", marginBottom: "1em", fontWeight: 700 }}
         >
           CS598: Deep Learning with Graphs
-        </b>
+        </div>
         <Typist>
           <div
             className="lead typist"
